dd4c316349dc0ed4e5aee50a2cc62ad49d04dbea
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-11-14 22:56:26 +0530

    Stack using linked list

--- STACKS/postfix_Evaluation.cpp
+++ STACKS/postfix_Evaluation.cpp
@@ -1,143 +1,135 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-
-template <class T>
 class Stack {
-private:
-    int size;
-    int top;
-    T* S;
+    private:
+        int size;
+        int top;
+        int* S;
+    public:
+        Stack(int size);
 
-public:
-    Stack(int size);
-    ~Stack();
-    void push(T x);
-    T pop();
-    T peek(int index);
-    int isFull();
-    int isEmpty();
-    void display();
-    T stackTop();
+        ~Stack();
+        void push(int x);
+        int pop();
+        int peek(int index);
+        int isFull();
+        int isEmpty();
+        void display();
+        int stackTop();
 };
+Stack::Stack(int size){
+        this->size = size;
+        top = -1;
+        S = new int[this->size];};
 
-template <class T>
-Stack<T>::Stack(int size) {
-    this->size = size;
-    top = -1;
-    S = new T[this->size];
-}
+Stack::~Stack(){
+            delete[] S;};
 
-template <class T>
-Stack<T>::~Stack() {
-    delete[] S;
-}
-
-template <class T>
-void Stack<T>::push(T x) {
-    if (isFull()) {
-        cout << "Stack Overflow" << endl;
-    } else {
+void Stack::push(int x){
+    if(isFull()){
+        cout<<"Stack Overflow"<<endl;}
+    else{
         top++;
-        S[top] = x;
-    }
-}
+        S[top]=x;
+    }}
 
-template <class T>
-T Stack<T>::pop() {
-    T x = 1;
-    if (isEmpty()) {
-        cout << "Stack Underflow" << endl;
-    } else {
-        x = S[top];
+int Stack::pop(){
+    int x = 1;
+    if (isEmpty()){
+        cout<<"Stack UNderflow"<<endl;}
+    else{
+        x=S[top];
         top--;
     }
     return x;
-}
+    }
 
-template <class T>
-T Stack<T>::peek(int index) {
-    T x = -1;
-    if (top - index + 1 < 0 || top - index + 1 == size) {
-        cout << "Invalid position!" << endl;
-    } else {
-        x = S[top - index + 1];
+int Stack::peek(int index){
+    int x = -1;
+    if(top-index+1 < 0 || top-index+1==size){
+        cout<<"INvalid position!"<<endl;
+    }
+    else{
+        x=S[top-index+1];
     }
     return x;
 }
 
-template <class T>
-int Stack<T>::isFull() {
-    if (top == size - 1) {
+int Stack::isFull(){
+    if(top == size-1){
         return 1;
     }
     return 0;
 }
 
-template <class T>
-int Stack<T>::isEmpty() {
-    if (top == -1) {
+int Stack::isEmpty(){
+    if(top == -1){
         return 1;
     }
     return 0;
 }
 
-template <class T>
-void Stack<T>::display() {
-    for (int i = top; i >= 0; i--) {
-        cout << S[i] << " | " << flush;
+void Stack::display(){
+    for(int i=top; i>=0;i--){
+        cout<<S[i]<<" | "<<flush;
     }
-    cout << endl;
+    cout<<endl;
 }
 
-template <class T>
-T Stack<T>::stackTop() {
-    if (isEmpty()) {
+int Stack::stackTop(){
+    if(isEmpty()){
         return -1;
     }
     return S[top];
 }
 
-// Parenthesis matching
-bool isBalanced(char *exp) {
-    Stack<char> stk((int)strlen(exp));
-    for (int i = 0; i < strlen(exp); i++) {
-        if (exp[i] == '(') {
+//parenthsis matching
+bool isBalanced(char *exp){
+    Stack stk((int)strlen(exp));
+    for(int i=0;i<strlen(exp);i++){
+        if(exp[i]=='('){
             stk.push(exp[i]);
-        } else if (exp[i] == ')') {
-            if (stk.isEmpty()) {
+        }
+        else if(exp[i]==')'){
+            if(stk.isEmpty()){
                 return false;
-            } else {
-                stk.pop();
             }
+            else{
+            stk.pop();}
         }
     }
     return stk.isEmpty() ? true : false;
-}
-
-int isOperand(char x) {
-    if (x == '+' || x == '-' || x == '*' || x == '/') {
-        return 0;
     }
-    return 1;
-}
 
-int performOperation(char op, int x, int y) {
-    switch (op) {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '*':
-            return x * y;
-        case '/':
-            return x / y;
-        default:
-            return -1;
+    int isoperand(char x){
+        if(x=='+'||x=='-'||x=='-'||x=='/'){
+            return 0;
+        }
+        return 1;
     }
-}
 
-int PostfixEvaluate(char *postfix) {
+    int operations(char op,int x,int y){
+        switch(op){
+            case '+':
+              return x+y;
+              break;
+            case '-':
+              return x-y;
+              break;
+            case '*':
+              return x*y;
+              break;
+            case '/':
+              return x/y;
+              break;
+            default:
+             return -1;
+        }
+    }
+
+   
+int Post_Evaluate(char *postfix) {
     Stack<int> stk;
     int x, y, result;
     for (int i = 0; postfix[i] != '\0'; i++) {
@@ -154,8 +146,9 @@ int PostfixEvaluate(char *postfix) {
     return result;
 }
 
-int main() {
-    char postfix[] = "35*45/+4-";
-    cout << PostfixEvaluate(postfix) << endl;
-    return 0;
-}
+    int main(){
+        char *postfix[]="35*45/+4-";
+        cout<<Postfix_Evaluate(postfix)<<endl;
+        return 0;
+
+    }
