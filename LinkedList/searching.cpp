15de00bdd6114a3c6ee53a93b06e92feb8126145
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-11-28 13:03:04 +0530

    serching in LL

--- LinkedList/searching.cpp
+++ LinkedList/searching.cpp
@@ -1,5 +1,26 @@
-#include <iostrem>
+#include <iostream>
 using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int A[],int n){
+    struct Node*t,*last;
+    first=new Node[sizeof(struct Node)];
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    for(int i=0;i<n;i++){
+        t=new Node[sizeof(struct Node)];
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
 struct Node* search(struct Node*p,int key){
     struct Node *q;
     while(p!=NULL){
@@ -14,3 +35,12 @@ struct Node* search(struct Node*p,int key){
     }
         return 0;
     }
+
+int main(){
+    int arr[]={34,34,21,53,764,98,9,23};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    create(arr,n);
+    struct Node *temp=search(first,34);
+    cout<<temp->data<<endl;
+    return 0;
+}
