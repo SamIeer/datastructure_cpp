8086ebccd2b8859cd75e81a000d4ea5154223b08
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-12-04 22:45:21 +0530

    checking for loop

---
+++ LinkedList/Isloop.cpp
@@ -0,0 +1,67 @@
+//Deletion 
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    struct Node *next;
+}*first=NULL,*second=NULL;
+
+void create(int A[],int n){
+    int i;
+    struct Node *t,*last;
+    first=new Node[sizeof(struct Node)];
+    first->data=A[0];
+    first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++){
+        t=new Node[sizeof(struct Node)];
+        t->data=A[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+    }
+}
+
+int count(struct Node *p){
+    int l=0;
+    while(p){
+        l++;
+        p=p->next;
+    }
+    return l;
+}
+
+int isloop(struct Node *f){
+    struct Node *p,*q;
+    p=q=first;
+    do{
+        p=p->next;
+        q=q->next;
+        q=q?q->next:q;
+    }while(p && q && p!=q);
+
+    if(p==q)
+     return 1;
+    else
+     return 0;
+}
+
+void Display(struct Node *p){
+    while(p!=NULL){
+        cout<<" "<<p->data;
+        p=p->next;
+    }
+}
+
+int main(){
+    struct Node *t1,*t2;
+    int A[]={10,20,30,40,50};
+    create(A,5);
+
+    t1=first->next->next;
+    t2=first->next->next->next->next;
+    t2->next=t1;
+
+    cout<<isloop(first);
+    return 0;
+}
