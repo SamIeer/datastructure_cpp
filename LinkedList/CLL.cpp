6a23b461c959d6a719c9b2a882111bc543201c94
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-12-08 23:03:30 +0530

    minor changes

--- LinkedList/CLL.cpp
+++ LinkedList/CLL.cpp
@@ -52,10 +52,51 @@ CircularLinkedList::~CircularLinkedList(){
         head=nullptr;
     }
 }
+int length(struct Node *p){
+    int len=0;
+    do{
+        len++;
+        p=p->next;
+    }while(p!=head);
+    return len;
+}
+
+int delete(struct Node *p,int index){
+    struct Node *q;
+    int i,x;
+    if(index<0||index>length(head))
+    return -1;
+    if(index==1){
+        while(p->next!=head)p=p->next;
+        x=head->data;
+        if(head==p){
+            free(head);
+            head=NULL;
+        }
+        else{
+            p->next=head->next;
+            free(head);
+            head=p->next;
+        }
+    }
+    else{
+        for(i=0;i<index-2;i++)
+        p=p->next;
+        q=p->next;
+        p->next=q->next;
+        x=q->data;
+        free(q);
+    }
+    return x;
+}
+
+
 
 int main(){
     int A[]={1,2,3,4,5,6};
     CircularLinkedList cl(A,sizeof(A)/sizeof(A[0]));
+    cl.Display();
+    delete(first,3);
     cl.Display();
     return 0;
 }
