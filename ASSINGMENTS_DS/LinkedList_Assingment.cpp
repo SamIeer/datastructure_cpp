910196e12fd195361056f9c6677622741e327076
Sameer Chauhan <sameerchauhan212204@gmail.com>
2023-12-19 19:20:20 +0530

    Insertion from end

--- ASSINGMENTS_DS/LinkedList_Assingment.cpp
+++ ASSINGMENTS_DS/LinkedList_Assingment.cpp
@@ -1,69 +1,50 @@
 #include <iostream>
 using namespace std;
-struct Node{
+
+struct Node {
     int data;
-    struct Node *next;
-}*first=NULL;
+    Node* next;
 
-int count(struct Node *p){
-    int l=0;
-    while(p){
-        l++;
-        p=p->next;
-    }
-    return l;
-}
+    // Constructor to initialize data and next pointer
+    Node(int value) : data(value), next(nullptr) {}
+};
 
-void create(int A[],int n){
-    struct Node *t,*last;
-    first=new Node[sizeof(struct Node)];
-    first->data=A[0];
-    first->next=NULL;
-    last=first;
-    for(int i=1;i<n;i++){
-        t=new Node[sizeof(struct Node)];
-        t->data=A[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
-    }
-    
-}
-
-void Display(struct Node *p){
-    while(p!=NULL){
-        cout<<"->"<<p->data;
-        p=p->next;
-    }
-}
-
-void Insert_end(struct Node*& p, int x) {
-    // Create a new node and allocate memory
-    struct Node* t = new Node(x);
-
+//Function to insert element in the end
+void Insert_end(Node*& p, int x) {
+    Node* t = new Node(x);
     if (p == nullptr) {
-        // If the linked list is empty, make the new node the head
         p = t;
         return;
     }
-
-    // Traverse to the end of the linked list
     Node* current = p;
     while (current->next != nullptr) {
         current = current->next;
     }
-
-    // Insert the new node at the end
     current->next = t;
 }
 
+void Display(Node* p) {
+    while (p != nullptr) {
+        cout << "->" << p->data;
+        p = p->next;
+    }
+    cout<<endl;
+}
 
 
+int main() {
+    Node* first = nullptr;
+    int A[] = {1, 2, 3, 4, 5};
+    int n = sizeof(A) / sizeof(A[0]);
 
-int main(){
-    int A[]={1,2,3,4,5,6,7,8,9,1234};
-    create(A,10);
-    Insert_end(first,5);
+    for (int i = 0; i < n; i++) {
+        Insert_end(first, A[i]);
+    }
+    Insert_end(first,7);
+
+    // Display the linked list
+    cout << "Linked List: ";
     Display(first);
+
     return 0;
 }
