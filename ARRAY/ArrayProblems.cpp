ba236d659d31d0d01ddead33bc3cbabbc0aba2d9
Sameer Chauhan <sameerchauhan212204@gmail.com>
2024-01-22 14:17:45 +0530

    Somes Problems in Array

--- ARRAY/ArrayProblems.cpp
+++ ARRAY/ArrayProblems.cpp
@@ -14,6 +14,7 @@ void Display(struct Array arr){
     cout<<endl;
 }
 
+//Function for merging two array
 void Merge_Arr(struct Array A,struct Array B){
         int n= B.length ,m=A.length ,i=0,j=0,k=0;
         int C[20];
@@ -31,6 +32,7 @@ void Merge_Arr(struct Array A,struct Array B){
 
         for(j ; j<n ; j++)
             C[k++]=B.A[j];
+            
        //DISPLAYING THE MERGED ARRAY
        for (int l = 0; l < m+n; l++)
        {
@@ -38,9 +40,76 @@ void Merge_Arr(struct Array A,struct Array B){
        }     
 }
 
+
+//Function to find missing a single missing element in the ARray
+//DIFF BETWEEN THE NUMBER AN ITS INDEX SHOULD BE CONSTANT;
+int single_missing(struct Array arr){
+       int Diff = arr.A[0] - 0;
+       for(int i=0; i<arr.length ; i++){
+         if(arr.A[i]-i != Diff){
+            return i + Diff;
+         }
+       }
+}
+
+
+//Function to find multiple missing element in the Array
+//UPDATE THE DIFFERENCE WHEN MISSING ELEMENT IS REACHED
+void Multiple_missing(struct Array arr){
+    int diff = arr.A[0] - 0;
+    for(int i=0 ; i<arr.length ; i++){
+        while(arr.A[i]-i > diff){
+            cout<< "at index :"<< i << " is " << i + diff << "\n" ;
+            diff++;}
+    }
+}
+
+
+//FINDING DUPLUCATE ELEMENT IN THE SORTEDD ARRAY
+void duplicate_S(struct Array arr){
+    int last_duplicate = 0;
+    for(int i=0 ; i<arr.length ; i++){
+        if(arr.A[i] == arr.A[i+1] && arr.A[i] != last_duplicate){
+               cout<<"The duplicate elements are:"<<arr.A[i]<<"\n";
+               last_duplicate = arr.A[i];}
+    }
+}
+
+
+ //FINDING DUPLICATE ELEMENTS IN NSORTED ARRAY
+void duplicate_US(struct Array arr){
+    for(int i=0 ; i<arr.length ; i++){
+         int count = 1;
+         if(arr.A[i] != -1){
+            for( int j =i+1; j <arr.length ; j++){
+                 if(arr.A[i] == arr.A[j]){
+                    count ++;
+                    arr.A[j] = -1;
+                 }
+            }
+            if(count > 1){
+                    cout <<"The duplicate elements are :"  <<arr.A[i] << "\n";
+                 }
+         }
+    }
+ }
+
 int main(){
-    struct Array A = {{1,3,5,23,67,56,87},10,7};
-    struct Array B = {{2,4,6,7,69,70,89},10,7};
-    Merge_Arr(A,B);
+    struct Array A = {{1,3,4,4,5,6,7,8,9,9},10,10};
+    struct Array B = {{2,2,6,69,69,70,89},10,7};
+    cout<< "\n The Merged Array : \n";
+    Merge_Arr(A,B); cout<<endl;
+
+    cout << "\n The single missing elemnt is :"<< single_missing(A);
+
+    cout << "\n The Multple missing elements are \n";
+    Multiple_missing(A);
+
+    cout<< "\n duplicate elements in sorted Array: \n";
+    duplicate_S(A);
+
+    cout<< "\n duplicate elements in Unsorted Array: \n";
+    duplicate_US(B);
+
     return 0;
 }
